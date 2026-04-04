26f61c11f7dc190ad362aa416846c92b7fa8385e
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2026-04-04 12:19:00 -0600

    Deleted List

--- api/src/main/java/com/cinemo/api/infrastructure/persistence/jpa/entity/StateEntity.java
+++ api/src/main/java/com/cinemo/api/infrastructure/persistence/jpa/entity/StateEntity.java
@@ -1,7 +1,5 @@
 package com.cinemo.api.infrastructure.persistence.jpa.entity;
 
-import java.util.List;
-
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
