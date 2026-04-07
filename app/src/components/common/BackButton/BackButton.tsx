4b05c25cccbbf5a566e1e7804e0a531a1c84d4af
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2026-04-07 14:16:02 -0600

    Added files

--- app/src/components/common/BackButton/BackButton.tsx
+++ app/src/components/common/BackButton/BackButton.tsx
@@ -1,21 +1,25 @@
-// components/movies/BackButtonMovies.tsx
 'use client';
 
 import { Button } from '@mantine/core';
 import { IconArrowLeft } from '@tabler/icons-react';
 import Link from 'next/link';
 
-export function BackButtonMovies() {
+interface BackButtonProps {
+  href: string;
+  label?: string;
+}
+
+export function BackButton({ href, label = 'Volver a la lista' }: BackButtonProps) {
   return (
     <Button
       component={Link}
-      href="/admin/movies"
+      href={href}
       variant="subtle"
       color="gray"
       leftSection={<IconArrowLeft size={16} />}
       w="fit-content"
     >
-      Volver a la lista
+      {label}
     </Button>
   );
 }
