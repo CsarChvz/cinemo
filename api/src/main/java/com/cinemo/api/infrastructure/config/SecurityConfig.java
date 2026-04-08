6b086374f8fe7443a2db71d57692b496fe6b8052
CsarChvz <cesarchavezrodriguez9@gmail.com>
2026-04-08 16:22:39 -0600

    IMplemented infrastructure thing

--- api/src/main/java/com/cinemo/api/infrastructure/config/SecurityConfig.java
+++ api/src/main/java/com/cinemo/api/infrastructure/config/SecurityConfig.java
@@ -1,5 +1,26 @@
 package com.cinemo.api.infrastructure.config;
 
+import com.cinemo.api.infrastructure.security.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
 
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/v1/auth/**").permitAll() // Registro y Login libres
+            .anyRequest().authenticated() // Todo lo demás bloqueado
+        )
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
+  }
 }
